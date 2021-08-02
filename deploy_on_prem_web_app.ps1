714cf94fb6e67d10ee0ece4850b3c5c3da609ac4
Roy Wong <roy.wong2@extendhealth.com>
2021-08-02 12:54:42 -0600

    Clean folder Code

--- deploy_on_prem_web_app.ps1
+++ deploy_on_prem_web_app.ps1
@@ -18,9 +18,9 @@ Param(
 Write-Output "Deploy On-Prem Web Application"
 Write-Output "Server: $server"
 
-$source_file_parts = $source_zip_file_path.Split('\')
-$source_file_name = $file_parts[$source_file_parts.Length - 1]
-$destination_zip_file_path = (Join-Path -Path $deployment_folder_path -ChildPath $source_file_name)
+# $source_file_parts = $source_zip_file_path.Split('\')
+# $source_file_name = $source_file_parts[$source_file_parts.Length - 1]
+# $destination_zip_file_path = (Join-Path -Path $deployment_folder_path -ChildPath $source_file_name)
 
 $credential = [PSCredential]::new($user_id, $password)
 $so = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
@@ -29,19 +29,29 @@ Write-Output "Importing remote server cert..."
 Import-Certificate -Filepath $cert_path -CertStoreLocation "Cert:\LocalMachine\Root"
 
 if ($clean_deployment_folder) {
-    Clear-Content $deployment_folder_path `
-        -ToSession $so -Credential $credential
+    $script = {
+        Param($clean_folder)
+        Write-Host "$clean_folder"
+        Remove-Item "$clean_folder\*.*"
+    }
+
+    Invoke-Command -ComputerName $server `
+        -Credential $credential `
+        -UseSSL `
+        -SessionOption $so `
+        -ScriptBlock $script `
+        -ArgumentList $deployment_folder_path
 }
 
-Copy-Item $source_zip_file_path `
-    -Destination $deployment_folder_path `
-    -ToSession $so -Credential $credential -Recurse
+# Copy-Item $source_zip_file_path `
+#     -Destination $deployment_folder_path `
+#     -ToSession $so -Credential $credential -Recurse
 
-Expand-Archive -Path $destination_zip_file_path `
-    -DestinationPath $deployment_folder_path `
-    -ToSession $so -Credential $credential
+# Expand-Archive -Path $destination_zip_file_path `
+#     -DestinationPath $deployment_folder_path `
+#     -ToSession $so -Credential $credential
 
-Remove-Item -Path $destination_zip_file_path `
-    -ToSession $so -Credential $credential
+# Remove-Item -Path $destination_zip_file_path `
+#     -ToSession $so -Credential $credential
 
 Write-Output "Web Application Files deployed."
